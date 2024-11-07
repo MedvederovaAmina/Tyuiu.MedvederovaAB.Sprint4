4353bfd8125b818066419d3bc393fac5ec5f24c7
Амина Медведерова <medvederovaamina@outlook.com>
2024-11-07 13:28:31 +0500

    Task3

---
+++ Tyuiu.MedvederovaAB.Sprint4.Task3.V26.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.MedvederovaAB.Sprint4.Task3.V26.Lib
+{
+    public class DataService : ISprint4Task3V26
+    {
+        public int Calculate(int[,] array)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] a = new int[m, n];
+            Random r = new Random();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = r.Next(0, 10);
+                    Console.Write(a[i, j] + " ");
+                }
+            }
+
+                return m;
+        }   
+    }
+}
---
+++ Tyuiu.MedvederovaAB.Sprint4.Task3.V26/Program.cs
@@ -0,0 +1,52 @@
+﻿using Tyuiu.MedvederovaAB.Sprint4.Task3.V26.Lib;
+
+namespace Tyuiu.MedvederovaAB.Sprint4.Task3.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4   |  Выполнила: Медведерова А. Б.   |  ПКТб-24-1";
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* Спринт #4                                                     *");
+            Console.WriteLine("* Тема: Одноименные массивы (генератор случайных чисел).        *");
+            Console.WriteLine("* Задание #2                                                    *");
+            Console.WriteLine("* Вариант #1                                                    *");
+            Console.WriteLine("* Выполнила: Медведерова Амина Борисовна | ПКТб-24-1            *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                      *");
+            Console.WriteLine("* Написать программу, которая считает произведение четных       *");
+            Console.WriteLine("* элементов массива.                                            *");
+            Console.WriteLine("*                                                               *");
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                              *");
+            Console.WriteLine("*****************************************************************");
+            Random rnd = new Random();
+            DataService ds = new DataService();
+
+
+            Console.Write("Введите количество элементов массива:");
+            int len = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[len];
+            for (int i = 0; i <= len - 1; i++)
+            {
+                array[i] = rnd.Next(1, 9);
+            }
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.Write(array[i] + "\t");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("*****************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                    *");
+            Console.WriteLine("*****************************************************************");
+            int res = ds.Calculate(array);
+            Console.WriteLine("Произведение четных элементов массива = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
